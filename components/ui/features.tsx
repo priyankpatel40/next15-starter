027b640b691331cd7886979eba0e3407389415b1
Priyank Patel <ppriyank40@gmail.com>
2024-09-26 16:22:18 +0530

    feat(Main webiste page imprvoements): Added FAQ, Features, Footer for main website

---
+++ components/ui/features.tsx
@@ -0,0 +1,45 @@
+import 'animate.css';
+
+import ITEMS from './items';
+
+const Features = () => {
+  return (
+    <div className="bg-gray-50 py-16 dark:bg-gray-900 sm:py-24 lg:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-indigo-600 dark:text-indigo-400 sm:text-4xl">
+            Features
+          </h2>
+        </div>
+        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {ITEMS.map((singleItem, index) => (
+            <div
+              key={singleItem.title}
+              className="animate__animated animate__fadeIn flex flex-col items-center justify-between rounded-2xl bg-white p-6 shadow-xl transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl dark:bg-gray-800 dark:shadow-gray-800"
+              style={{ animationDelay: `${index * 0.1}s` }}
+            >
+              <div className="mb-2 flex size-16 items-center justify-center rounded-full bg-indigo-100 dark:bg-indigo-900">
+                {singleItem.icon}
+              </div>
+              <div className="text-center">
+                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                  {singleItem.title}
+                </h3>
+                <p className="text-base text-gray-600 dark:text-gray-400">
+                  {singleItem.description}
+                </p>
+              </div>
+              {singleItem.comingSoon && (
+                <span className="mt-4 inline-flex items-center rounded-full bg-blue-100 px-3 py-0.5 text-sm font-medium text-blue-800 dark:bg-blue-800 dark:text-blue-100">
+                  Coming Soon
+                </span>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
---
+++ components/ui/footer.tsx
@@ -0,0 +1,58 @@
+import { LinkedInLogoIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
+import type { ReactElement } from 'react';
+import { FaGithub, FaTwitter } from 'react-icons/fa';
+
+const FooterLink = ({ href, text }: { href: string; text: string }) => (
+  <div className="px-5 py-2">
+    <a
+      href={href}
+      className="text-base leading-6 text-gray-600 transition-colors duration-300 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white"
+    >
+      {text}
+    </a>
+  </div>
+);
+
+const SocialLink = ({
+  href,
+  label,
+  icon,
+}: {
+  href: string;
+  label: string;
+  icon: ReactElement;
+}) => (
+  <Link
+    href={href}
+    className=" text-gray-500 transition-colors duration-300 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300"
+  >
+    <span className="sr-only">{label}</span>
+    {icon}
+  </Link>
+);
+
+const FooterComponent = () => (
+  <footer className="bg-gray-100 dark:bg-gray-800">
+    <div className="mx-auto max-w-screen-xl space-y-8 overflow-hidden px-4 py-12 sm:px-6 lg:px-8">
+      <nav className="-mx-5 -my-2 flex flex-wrap justify-center">
+        <FooterLink href="#" text="About" />
+        <FooterLink href="#" text="Pricing" />
+        <FooterLink href="#" text="Contact" />
+        <FooterLink href="#" text="Terms" />
+      </nav>
+
+      <div className="mt-8 flex justify-center space-x-6 ">
+        <SocialLink
+          href="https://www.linkedin.com/in/priyankpatel2828/"
+          label="LinkedIn"
+          icon={<LinkedInLogoIcon />}
+        />
+        <SocialLink href="https://x.com/ppriyank40" label="X" icon={<FaTwitter />} />
+        <SocialLink href="https://x.com/ppriyank40" label="X" icon={<FaGithub />} />
+      </div>
+    </div>
+  </footer>
+);
+
+export default FooterComponent;
