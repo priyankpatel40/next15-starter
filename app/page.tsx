e5fe8d6a5518cc7ec4a8f1e197449be098efe874
Priyank Patel <ppriyank40@gmail.com>
2024-09-26 16:35:09 +0530

    style(Responsive ): Improvements for responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
 export default function Web() {
   return (
     <div className="dark:bg-gray-900">
-      <section className="animate__animated animate__fadeIn bg-[#fafafa] bg-custom-bg bg-contain bg-right-top bg-no-repeat pb-14 transition duration-500 ease-in-out dark:from-gray-800 dark:to-gray-600">
+      <section className="animate__animated animate__fadeIn bg-[#fafafa]  bg-contain bg-right-top bg-no-repeat pb-14 transition duration-500 ease-in-out dark:from-gray-800 dark:to-gray-600 lg:bg-custom-bg">
         <div className="mx-auto flex items-center justify-end p-4">
           <Link
             href="/login"
@@ -45,26 +45,26 @@ export default function Web() {
             <ThemeSwitch />
           </div>
         </div>
-        <div className="w-1/2 px-4 py-8 text-left lg:py-16">
+        <div className="w-full px-4 py-8 text-left lg:w-1/2 lg:py-16">
           <div className="mx-auto place-self-start">
-            <h1 className="animate__animated animate__fadeIn mb-4 text-5xl font-extrabold leading-tight tracking-tight text-black dark:text-black md:text-5xl xl:text-6xl">
+            <h1 className="animate__animated animate__fadeIn mb-4 text-4xl font-extrabold leading-tight tracking-tight text-black dark:text-black md:text-5xl xl:text-6xl">
               Next.js Enterprise Starter Kit
             </h1>
 
-            <h2 className="mb-6 p-4 text-left text-3xl font-extrabold text-black dark:text-black">
+            <h2 className="mb-6 p-4 text-left text-2xl font-extrabold text-black dark:text-black md:text-3xl">
               Launch Your Enterprise Product{' '}
-              <span className="animate-pulse-glow rounded-md bg-gradient-to-r from-orange-500 to-pink-500 px-3 py-2 text-white">
+              <span className="animate-pulse-glow rounded-md bg-gradient-to-r from-orange-500 to-pink-500 px-2  text-white">
                 In Hours
               </span>{' '}
               with Next.js Starter Kits
             </h2>
 
-            <p className="mb-6 max-w-lg text-left text-lg text-black dark:text-black">
+            <p className="mb-6 max-w-lg text-left text-base text-black dark:text-black md:text-lg">
               Accelerate your development process with our robust and scalable starter kit
               designed for enterprise applications.
             </p>
 
-            <div className="flex items-center space-x-4">
+            <div className="flex flex-col items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0">
               <Button className="flex items-center rounded-lg bg-gradient-to-r from-green-400 to-blue-500 px-6 py-3 text-lg text-white transition-transform hover:from-green-500 hover:to-blue-600">
                 <GitHubLogoIcon className="mr-2 size-6" />
                 <Link
